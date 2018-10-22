fee89c2480f062750ba8366c496c035160e3d355
Neareeves <alethesj@gmail.com>
2018-10-22 16:52:49 +0200

    commit les missiles ne fonctionnent pas

--- JS/javAleth.js
+++ JS/javAleth.js
@@ -1,3 +1,22 @@
 function
 
-// parseInt(indow.getComputedStyle(var).height) + 10 + "px"
+// parseInt(window.getComputedStyle(var).height) + 10 + "px"
+
+document.onkeydow = function (event) {
+    //console.log (event.keyCode);
+  if (event.keycode == 103);  {    //Déplacement haut gauche
+  vaisseau.left = vaisseau.left - 10;  
+  vaisseau.top = vaisseau.top - 10;
+    }
+};
+
+if (event.keyCode == 101 ||  event.keycode == 32) {
+    missile.display="block";
+    missile.left = vaisseau.left + (vaisseau._node.width - missible._node.width) /2;
+    missile.top = vaisseu.top;
+}
+
+```
+
+```
+var missile= missile.display= "none";
